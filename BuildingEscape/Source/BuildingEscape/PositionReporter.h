f403d7210a0ac7d6881bf119c997de9e2f5d2bd9
Phillip <psturtevant400@gmail.com>
2017-03-21 16:07:28 -0700

    Code Update
    Generalized OpenDoor method so activates during certain conditions.

--- BuildingEscape/Source/BuildingEscape/PositionReporter.h
+++ BuildingEscape/Source/BuildingEscape/PositionReporter.h
@@ -22,7 +22,4 @@ protected:
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
-
-		
-	
 };
