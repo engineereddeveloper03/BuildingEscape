196a9bb5d97a68a6ccbf3e1c3e2b758617c934f8
Phillip <psturtevant400@gmail.com>
2017-03-22 11:23:24 -0700

    Added Output for Location & Rotation
    Added an output line to constantly update position and location of pawn.

--- BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -3,6 +3,7 @@
 #include "BuildingEscape.h"
 #include "Grabber.h"
 
+#define OUT // Annotates to show parameters are changed bymethod
 
 // Sets default values for this component's properties
 UGrabber::UGrabber()
@@ -20,8 +21,7 @@ void UGrabber::BeginPlay()
 {
 	Super::BeginPlay();
 
-	UE_LOG(LogTemp, Warning, TEXT("Grabber reporting for duty!"));
-	
+	UE_LOG(LogTemp, Warning, TEXT("Grabber reporting for duty!"))
 }
 
 
@@ -30,6 +30,25 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	// Get player view point this tick
+	FVector PlayerViewPointLocation;
+	FRotator PlayerViewPointRotation;
+
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+		OUT PlayerViewPointLocation,
+		OUT PlayerViewPointRotation
+	);
+
+	// Log out to test
+	UE_LOG(LogTemp, Warning, TEXT("Location: %s, Rotation: %s"), 
+		*PlayerViewPointLocation.ToString(), 
+		*PlayerViewPointRotation.ToString()
+	)
+
+	// Ray-cast out to reach distance
+
+
+	// See what we hit
+
 }
 
